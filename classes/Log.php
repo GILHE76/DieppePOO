60c3e1565706b3eb10e513fb387ba5586a51bce0
Bob Marley <bob@marley.com>
2018-04-26 12:02:04 +0200

    ecriture de chaine de caractere passé dans le fichier horodaté.

---
+++ classes/Log.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dell
+ * Date: 26/04/2018
+ * Time: 12:00
+ */
+
+{
+    public static function logWrite($data)
+{
+    $directory = "/logs/";
+    $file = date('Y-m-d')  . ".log";
+    $path = dirname(__DIR__) . $directory . $file;
+    $data = date('H:i:s:u') . " " . $data;
+    $handle = fopen($path, "a");
+    if (flock($handle, LOCK_EX))
+    {
+        fwrite($handle, $data . PHP_EOL);
+        flock($handle, LOCK_UN);
+    }
+    fclose($handle);
+}
+}
