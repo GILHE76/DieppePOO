97e6d624b34e7bbcfb81715a851978e134610ee8
Bob Marley <bob@marley.com>
2018-05-03 11:10:50 +0200

    ajout de la date inclusion de l'autoloader Instanciation de l'objet

--- index.php
+++ index.php
@@ -4,3 +4,6 @@ date_default_timezone_set("Europe/Paris");
 include "./functions/classAutoLoader.php";
 spl_autoload_register('classAutoLoader');
 
+$voiture = new Berline("citroen", "saxo");
+
+var_dump($voiture);
